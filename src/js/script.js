40da56ffd42a710fd42e00cedf604d3994994363
leomark92 <markobogush@gmail.com>
2021-02-17 16:10:26 +0200

    fs1

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+const line = document.querySelector('.deli'),
+      overlay = document.querySelector('.overlay'),
+      menu = document.querySelector('.menu'),
+      xclose = document.querySelector('.close');
+
+line.addEventListener('click', function(){
+    overlay.classList.toggle('active')
+    menu.classList.toggle('menu-active')
+    
+});
+
+xclose.addEventListener('click', function(){
+    overlay.classList.toggle('active')
+    menu.classList.toggle('menu-active')
+});
+
+
