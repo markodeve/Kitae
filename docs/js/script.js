4bfaf57a8994fe7011d4259a31289e328d2173d7
leomark92 <markobogush@gmail.com>
2021-02-23 17:38:23 +0200

    modal+sscroll

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,7 +1,7 @@
 const line = document.querySelector('.deli'),
     overlay = document.querySelector('.overlay'),
     menu = document.querySelector('.menu'),
-    
+    headerMenu = document.querySelectorAll('.popup'),
     xclose = document.querySelector('.close');
 
 line.addEventListener('click', function () {
@@ -10,10 +10,60 @@ line.addEventListener('click', function () {
 
 });
 
+$(headerMenu).on('click', function(){
+    overlay.classList.remove('active');
+    menu.classList.remove('menu-active');
+
+});
 
 
 xclose && overlay.addEventListener('click', function () {
     overlay.classList.remove('active');
     menu.classList.remove('menu-active');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+    } else {
+        $('.pageup').fadeOut();
+    }
+});
+
+$("a[href^='#']").click(function(){
+    var _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+});
+
+
+$('[data-modal=head]').on('click', function(){
+    $('.overlay__modal').fadeIn('fast');
+
+});
+
+$('.modal__close').on('click', function(){
+    $('.overlay__modal').fadeOut('fast');
+});
+
 
